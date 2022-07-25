d5e7402ae46c4ddef5222b6804d49508adafd79b
Riyad Mney <rimney@e1r5p10.1337.ma>
2022-07-25 02:40:31 +0100

    dd

--- Execution/redirections/ft_append.c
+++ Execution/redirections/ft_append.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_append.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
+/*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 15:57:20 by rimney            #+#    #+#             */
-/*   Updated: 2022/07/23 22:03:23 by rimney           ###   ########.fr       */
+/*   Updated: 2022/07/25 02:40:15 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,38 +42,48 @@ void	ft_advanced_append(int index, t_exec *exec, int fd_out, int fd_in, int loca
 	WIFEXITED(exec->env.exit_value);
 }
 
-
-
-int	ft_append(int index, t_exec *exec, int command_location)
+void	ft_append_final_case(t_exec *exec, int index, char **parser, int in)
 {
 	int fd;
-	int in;
 	int pid;
-	int s_flag;
-	char **parser;
 
-	s_flag = 0;
-	parser = ft_split(exec->command[1], ' ');
-	if(index == 0 && ft_is_another_flag(exec, index) == APPEND)
+
+	fd = open(parser[0], O_CREAT | O_RDWR | O_APPEND, 0644);
+	ft_find_next_flag(exec, &index, &fd, &in);
+	pid = fork();
+	if(pid == 0)
 	{
-		fd = open(parser[0], O_CREAT | O_RDWR | O_APPEND, 0644);
-		ft_find_next_flag(exec, &index, &fd, &in);
-		pid = fork();
-		if(pid == 0)
-		{
-			dup2(fd, 1);
-			close(fd);
-			execve(ft_exec_command(exec->envp, parser[1]), parser + 1, exec->envp);
-		}
-		else
-			waitpid(pid, 0, 0);
-		index = 2;
-		exec->in = fd;
+		dup2(fd, 1);
+		close(fd);
+		execve(ft_exec_command(exec->envp, parser[1]), parser + 1, exec->envp);
 	}
 	else
+		waitpid(pid, 0, 0);
+}
+
+
+int	ft_append_child(t_exec *exec, int in, int fd, int command_location)
+{
+	exec->in = fd;
+	if(exec->input_flag || exec->heredoc_flag)
 	{
-		index = 1;
-		command_location = 0;
+		dup2(in, 0);
+		close(in);
+		exec->input_flag = 0;
+		exec->heredoc_flag = 0;
+	}
+	dup2(fd, 1);
+	close(fd);
+	ft_execute_command(exec, command_location);
+	return (1);
+}
+
+void	ft_append_norm(t_exec *exec, int index, int command_location, int s_flag)
+{
+	int in;
+	int fd;
+	int pid;
+
 	while(index < exec->append_count)
 	{
 		fd = ft_open(exec, APPEND, index + 1);
@@ -82,31 +92,43 @@ int	ft_append(int index, t_exec *exec, int command_location)
 		if(exec->error_flag)
 		{
 			exec->error_flag = 0;
-			return (0);
+			return ;
 		}
 		if((index + 1 == exec->append_count || s_flag == 1))
 		{
 			pid = fork();
 			if(pid == 0)
-			{
-				if(exec->input_flag || exec->heredoc_flag)
-				{
-					dup2(in, 0);
-					close(in);
-					exec->input_flag = 0;
-					exec->heredoc_flag = 0;
-				}
-				dup2(fd, 1);
-				close(fd);
-				ft_execute_command(exec, command_location);
-			}
+				ft_append_child(exec, in, fd, command_location);
 		}
 		index += 2;
 	}
-
 	waitpid(pid, &exec->env.exit_value, 0);
 	WIFEXITED(exec->env.exit_value);
-	exec->in = fd;
+}
+
+int	ft_append(int index, t_exec *exec, int command_location)
+{
+	int fd;
+	int s_flag;
+	char **parser;
+	int in;
+
+	s_flag = 0;
+	in = -1;
+	fd = -1;
+	parser = ft_split(exec->command[1], ' ');
+	if(index == 0 && ft_is_another_flag(exec, index) == APPEND)
+	{
+		ft_append_final_case(exec, index, parser, in);
+		index = 2;
+		exec->in = fd;
+	}
+	else
+	{
+		index = 1;
+		command_location = 0;
+		ft_append_norm(exec, index, command_location, s_flag);
+		index += 2;
 	}
 	ft_free(parser);
 	return (index);
